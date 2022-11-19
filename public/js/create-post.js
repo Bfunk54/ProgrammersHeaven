2f897766779cf15d7131ca73d8dbfe838019acef
Bfunk54 <ben_fein@icloud.com>
2022-11-18 23:45:27 -0500

    added delete-post js

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -1,47 +1,27 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#post-name').value.trim();
-  const content = document.querySelector('#post-content').value.trim();
+  const name = document.querySelector("#post-name").value.trim();
+  const content = document.querySelector("#post-content").value.trim();
   console.log(content);
-  console.log(JSON.stringify({name, content}));
+  console.log(JSON.stringify({ name, content }));
   if (name && content) {
     const response = await fetch(`/api/create-post`, {
-      method: 'POST',
-      body: JSON.stringify({name, content}),
+      method: "POST",
+      body: JSON.stringify({ name, content }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace("/dashboard");
     } else {
-      alert('Failed to create project');
+      alert("Failed to create project");
     }
   }
 };
 
-// const delButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-id')) {
-//     const id = event.target.getAttribute('data-id');
-
-//     const response = await fetch(`/api/projects/${id}`, {
-//       method: 'DELETE',
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert('Failed to delete project');
-//     }
-//   }
-// };
-
 document
-  .querySelector('.new-post-form')
-  .addEventListener('submit', newFormHandler);
-
-// document
-//   .querySelector('.project-list')
-//   .addEventListener('click', delButtonHandler);
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
