ddfd3388d7e6454cadf7d91012d7b2d6964e3128
Bfunk54 <ben_fein@icloud.com>
2022-11-19 10:36:55 -0500

    finally got the comment post to work!!!!

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
-const { Comment } = require("../../models/");
+const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
-      ...req.body,
-      userId: req.session.userId,
+      comment: req.body.body,
+      post_id: req.body.postId,
+      user_id: req.session.user_id,
     });
+    newComment;
+    
+    console.log(newComment);
     res.json(newComment);
   } catch (err) {
     res.status(500).json(err);
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,25 +1,29 @@
 const commentFormHandler = async function (event) {
-  event.preventDefault();
+//   event.preventDefault();
 
-  const postId = document.querySelector('input[name="post-id"]').value;
-  const body = document.querySelector('textarea[name="comment-body"]').value;
-
+  const postId = document.querySelector('.post-id').textContent;
+  const body = document.querySelector('.comment-body').value;
+  console.log(body);
+  console.log(postId);
   if (body) {
-    await fetch("/api/comment", {
+    
+   const response = await fetch(`/api/comment`, {
       method: "POST",
-      body: JSON.stringify({
-        postId,
-        body,
-      }),
+      body: JSON.stringify({ postId, body }),
       headers: {
         "Content-Type": "application/json",
       },
     });
 
+    if (response.ok) {
     document.location.reload();
+  }else {
+    alert("Failed to create comment");
   }
+}
 };
 
+console.log(document.querySelector("#new-comment-form"));
 document
   .querySelector("#new-comment-form")
-  .addEventListener("submit", commentFormHandler);
+  .addEventListener("click", commentFormHandler);
