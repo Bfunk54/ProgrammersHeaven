641128d5a5619372d1f8c5141e4aa74f688287d2
Bfunk54 <ben_fein@icloud.com>
2022-11-19 01:35:40 -0500

    deleting a post now works from the dashboard

--- public/js/delete-post.js
+++ public/js/delete-post.js
@@ -2,14 +2,14 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute("data-id")) {
     const id = event.target.getAttribute("data-id");
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/create-post/${id}`, {
       method: "DELETE",
     });
 
     if (response.ok) {
-      document.location.replace("/profile");
+      document.location.replace("/dashboard");
     } else {
-      alert("Failed to delete project");
+      alert("Failed to delete post");
     }
   }
 };
